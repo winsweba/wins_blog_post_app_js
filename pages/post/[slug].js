6f1e1742b56b614d8b49b256244e0988401ba029
winsweba <winsweb@outlook.com>
2022-02-28 19:49:37 -0800

    making  effect

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -56,6 +56,6 @@ export async function getStaticPaths() {
   const posts = await getPosts();
   return {
     paths: posts.map(({ node: { slug } }) => ({ params: { slug } })),
-    fallback: true,
+    fallback: false,
   };
 }
