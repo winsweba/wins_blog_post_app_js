1c0cfd1d532395704e6ddde03f28e20629112ab6
winsweba <winsweb@outlook.com>
2022-02-28 19:01:13 -0800

    making  effect

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -40,18 +40,22 @@ const PostDetails = ({post}) => {
 
 export default PostDetails;
 
-export async function getStaticProps({params}) {
+// Fetch data at build time
+export async function getStaticProps({ params }) {
   const data = await getPostDetails(params.slug);
   return {
-    props: { post: data },
+    props: {
+      post: data,
+    },
   };
 }
 
+// Specify dynamic routes to pre-render pages based on data.
+// The HTML is generated at build time and will be reused on each request.
 export async function getStaticPaths() {
-    const posts = await getPosts();
-
-    return {
-        paths: posts.map(({ node: {slug}}) => ({params: {slug}})),
-        fallback: true,
-    }
+  const posts = await getPosts();
+  return {
+    paths: posts.map(({ node: { slug } }) => ({ params: { slug } })),
+    fallback: true,
+  };
 }
